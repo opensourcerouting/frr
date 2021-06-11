5fb06de863190ff214c0044a9ebfa18e7182c1ca
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-06-11 11:51:53 -0300

    zebra: support multicast southbound
    Add code to support multicast route installation replacement for `MFC_*`
    operations.
    
    Signed-off-by: Rafael Zalamena <rzalamena@opensourcerouting.org>

--- zebra/zebra_mroute.h
+++ zebra/zebra_mroute.h
@@ -7,6 +7,8 @@
 #ifndef __ZEBRA_MROUTE_H__
 #define __ZEBRA_MROUTE_H__
 
+#include <linux/mroute.h>
+
 #include "zebra/zserv.h"
 
 #ifdef __cplusplus
@@ -22,6 +24,70 @@ struct mcast_route_data {
 };
 
 void zebra_ipmr_route_stats(ZAPI_HANDLER_ARGS);
+
+/** Custom netlink attribute value definition. */
+#define RTA_MRT_EXTRA 64
+
+/** Custom netlink attribute for passing extra multicast route information. */
+struct mrt_extra_attr {
+	/** Bandwidth threshold in kbps. */
+	int32_t spt_threshold;
+	/** RPF interface index. */
+	int32_t notif_idx;
+	/** Multicast flags. */
+	uint32_t flags;
+	/** RP encapsulated data: source. */
+	struct in6_addr local;
+	/** RP encapsulated data: destination. */
+	struct in6_addr remote;
+};
+
+/** Multicast output interface argument. */
+struct mroute_oif_arg {
+	SLIST_ENTRY(mroute_oif_arg) entry;
+
+	/** Interface index. */
+	ifindex_t index;
+};
+SLIST_HEAD(mroute_oif_list, mroute_oif_arg);
+
+struct mroute_oif_arg *mroute_oif_arg_new(struct mroute_oif_list *oif_list);
+void mroute_oif_arg_free(struct mroute_oif_list *oif_list, struct mroute_oif_arg **oif);
+void mroute_oif_list_free_all(struct mroute_oif_list *oif_list);
+
+/** Multicast route argument represantation. */
+struct mroute_args {
+	/** Source address. */
+	struct ipaddr source;
+	/** Multicast group address. */
+	struct ipaddr group;
+	/** Flags to signalize different options. */
+	uint32_t flags;
+	/** Input interface index. */
+	ifindex_t input;
+	/** Amount of output interfaces. */
+	size_t output_amount;
+	/** Output interface indexes. */
+	struct mroute_oif_list oif_list;
+	/** RPF interface information. */
+	ifindex_t notif_idx;
+
+	/** Bandwidth threshold information (in kbps). */
+	int32_t spt_threshold;
+
+	/** RP encap local information. */
+	struct ipaddr local;
+	/** RP encap remote information. */
+	struct ipaddr remote;
+
+	/** Multicast route operation. */
+	/* enum dplane_op_e */ int mroute_op;
+
+	/** VRF identification. */
+	vrf_id_t vrf_id;
+};
+
+void zmroute_event(ZAPI_HANDLER_ARGS);
 
 #ifdef __cplusplus
 }
