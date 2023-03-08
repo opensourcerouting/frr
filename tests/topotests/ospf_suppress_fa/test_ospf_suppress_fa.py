0e1892569f8b9172236e08c4b4bf882ba3b033f0
Renato Westphal <renato@opensourcerouting.org>
2023-03-07 21:13:53 -0300

    ospfd: refactor the "area nssa" command using DEFPY
    Combine all variation of the "area nssa" command into a single
    DEFPY to improve code maintainability.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- tests/topotests/ospf_suppress_fa/test_ospf_suppress_fa.py
+++ tests/topotests/ospf_suppress_fa/test_ospf_suppress_fa.py
@@ -112,7 +112,7 @@ def ospf_unconfigure_suppress_fa(router_name, area):
     tgen = get_topogen()
     router = tgen.gears[router_name]
     router.vtysh_cmd(
-        "conf t\nrouter ospf\nno area {} nssa suppress-fa\nexit\n".format(area)
+        "conf t\nrouter ospf\narea {} nssa\nexit\n".format(area)
     )
 
 
