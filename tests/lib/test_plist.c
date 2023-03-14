cd71322eec4ad3a1c555ef0a123efc73c831e30c
David Lamparter <equinox@opensourcerouting.org>
2023-03-14 21:22:41 +0100

    tests: add file load to test_plist

--- tests/lib/test_plist.c
+++ tests/lib/test_plist.c
@@ -11,6 +11,23 @@
 #include "lib/plist.h"
 #include "lib/filter.h"
 #include "tests/lib/cli/common_cli.h"
+#include "tests/lib/test_plist_clippy.c"
+
+DEFPY(load_config, load_config_cmd,
+      "load-config FILENAME",
+      "load config\nfile name\n")
+{
+	bool result;
+	struct timeval t1;
+	int64_t t;
+
+	vty_out(vty, "loading %s\n", filename);
+	monotime(&t1);
+	result = vty_read_config(NULL, filename, (char *)"/var/lib/nonexistent");
+	t = monotime_since(&t1, NULL);
+	vty_out(vty, "done, result=%d, time=%lldµs\n", result, (long long)t);
+	return CMD_SUCCESS;
+}
 
 static const struct frr_yang_module_info *const my_yang_modules[] = {
 	&frr_filter_info,
@@ -28,6 +45,7 @@ void test_init(int argc, char **argv)
 	prefix_list_init();
 	filter_cli_init();
 
+	install_element(ENABLE_NODE, &load_config_cmd);
 	/* nothing else to do here, giving stand-alone access to the prefix
 	 * list code's "debug prefix-list ..." command is the only purpose of
 	 * this "test".
