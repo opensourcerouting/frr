6b24f1e45caba9bf7e2c74bf90079ec9da40788e
Bruno Bernard <contact.brunobernard@gmail.com>
2023-11-20 14:52:33 +0400

    topotato: add scripts for vagrant
    Signed-off-by: Bruno Bernard <contact.brunobernard@gmail.com>

---
+++ vm/freebsd/install.sh
@@ -0,0 +1,41 @@
+#!/usr/local/bin/bash
+
+pkg install git autoconf automake libtool gmake json-c pkgconf \
+    bison py39-pytest c-ares py39-sphinx texinfo libunwind libyang2
+
+pw groupadd frr -g 101
+pw groupadd frrvty -g 102
+pw adduser frr -g 101 -u 101 -G 102 -c "FRR suite" \
+    -d /usr/local/etc/frr -s /usr/sbin/nologin
+
+
+cd /home/vagrant
+git clone https://github.com/frrouting/frr.git --single-branch frr
+cd frr
+./bootstrap.sh
+export MAKE=gmake LDFLAGS=-L/usr/local/lib CPPFLAGS=-I/usr/local/include
+./configure \
+    --sysconfdir=/usr/local/etc/frr \
+    --enable-pkgsrcrcdir=/usr/pkg/share/examples/rc.d \
+    --localstatedir=/var/run/frr \
+    --prefix=/usr/local \
+    --enable-multipath=64 \
+    --enable-user=frr \
+    --enable-group=frr \
+    --enable-vty-group=frrvty \
+    --enable-configfile-mask=0640 \
+    --enable-logfile-mask=0640 \
+    --enable-fpm \
+    --with-pkg-git-version \
+    --with-pkg-extra-version=-MyOwnFRRVersion
+gmake
+gmake check
+gmake install
+
+mkdir /usr/local/etc/frr
+touch /usr/local/etc/frr/frr.conf
+
+sysctl -w net.inet.ip.forwarding=1
+sysctl -w net.inet6.ip6.forwarding=1
+
+service sysctl restart
