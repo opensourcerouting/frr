5024f828029042fb509acfa65f482368a1ec44aa
Don Fedyk <dfedyk@labn.net>
2025-11-28 11:18:43 -0500

    ospfd: implement rfc 4222 rec 1 classify packet high/low prio
    Implement RFC 4222 Rec 1: classify OSPF packets into
    high-priority (Hellos, LSA Acks) and low-priority classes,
    and prioritize high-priority processing to maintain critical
    control packet handling during congestion. All control
    packet can have a DSCP value set, or explicitly low control
    can have a different DSCP value.
    
    Signed-off-by: Don Fedyk <dfedyk@labn.net>

--- ospfd/ospf_packet.c
+++ ospfd/ospf_packet.c
@@ -416,6 +416,34 @@ void ospf_ls_ack_delayed_timer(struct event *event)
 		ospf_ls_ack_send_delayed(oi);
 }
 
+static inline uint8_t ospf_tos_for_type(struct ospf_interface *oi, uint8_t type)
+{
+	uint8_t dscp;
+
+	switch (type) {
+	case OSPF_MSG_HELLO:
+	case OSPF_MSG_LS_ACK:
+		/* Always highest priority */
+		dscp = OSPF_IF_PARAM(oi, dscp_ospf_all);
+		break;
+
+	case OSPF_MSG_DB_DESC:
+	case OSPF_MSG_LS_REQ:
+	case OSPF_MSG_LS_UPD:
+		/* Bulk control traffic: low control */
+		dscp = OSPF_IF_PARAM(oi, dscp_low_control);
+		break;
+
+	default:
+		dscp = OSPF_IF_PARAM(oi, dscp_low_control);
+		break;
+	}
+
+	dscp &= 0x3f;		     /* clamp to 6 bits */
+	return (uint8_t)(dscp << 2); /* DSCP in bits 7..2, ECN = 0 */
+}
+
+
 #ifdef WANT_OSPF_WRITE_FRAGMENT
 static void ospf_write_frags(int fd, struct ospf_packet *op, struct ip *iph,
 			     struct msghdr *msg, unsigned int maxdatasize,
@@ -593,7 +621,8 @@ static void ospf_write(struct event *event)
 					overflow ip_hl.. */
 
 		iph.ip_v = IPVERSION;
-		iph.ip_tos = IPTOS_PREC_INTERNETCONTROL;
+		/* RFC4222 allow Hellos to have higher priority */
+		iph.ip_tos = ospf_tos_for_type(oi, type);
 		iph.ip_len = (iph.ip_hl << OSPF_WRITE_IPHL_SHIFT) + op->length;
 
 #if defined(__DragonFly__)
