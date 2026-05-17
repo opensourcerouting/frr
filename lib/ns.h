a0dc804584f61d6d266ca1e2af1d03eabf5e624c
David 'equinox' Lamparter <equinox@opensourcerouting.org>
2026-05-17 20:42:16 +0200

    wtf

--- lib/ns.h
+++ lib/ns.h
@@ -104,10 +104,10 @@ extern void ns_init_management(ns_id_t ns_id, ns_id_t internal_ns_idx);
 
 /* Create a socket serving for the given NS
  */
+int ns_socket(int domain, int type, int protocol, ns_id_t ns_id);
+
 #ifdef __linux__
 #define ns_socket psep_netns_socket
-#else
-int ns_socket(int domain, int type, int protocol, ns_id_t ns_id);
 #endif
 
 /* return the path of the NETNS */
